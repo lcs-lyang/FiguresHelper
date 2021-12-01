4d5b0e86160be2f4c77aa09e049c6ee855452268
Lillian Yang <lyang@lcs.on.ca>
2021-12-01 09:54:34 -0500

    Initial Commit

---
+++ FiguresHelper/FiguresHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiguresHelperApp.swift
+//  FiguresHelper
+//
+//  Created by Lillian Yang on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct FiguresHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
